c2bdebcc383f48c4f7e52263b5ee88b43d4a1011
Minho Jang <deakse2@gmail.com>
2025-12-18 13:02:23 -0800

    Configure data tier route table and NACL

--- routing.tf
+++ routing.tf
@@ -50,20 +50,36 @@ resource "aws_route_table" "private_rt" {
     }
 }
 
+resource "aws_route_table" "private_data_rt" {
+    vpc_id = aws_vpc.demo_vpc.id
+
+    tags = {
+        Name = "rt-private-data"
+        Tier = "data"
+    }
+}
+
 
 # ------------------------
 # Route Associations
 # ------------------------
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_assoc" {
   for_each = aws_subnet.public
 
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "private_assoc" {
     for_each = aws_subnet.private
 
     subnet_id = each.value.id
     route_table_id = aws_route_table.private_rt[each.key].id
+}
+
+resource "aws_route_table_association" "private_data_assoc" {
+    for_each = aws_subnet.private_data
+
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.private_data_rt[each.key].id
 }
--- security.tf
+++ security.tf
@@ -1,4 +1,70 @@
 # ------------------------
+# NACL
+# ------------------------
+resource "aws_network_acl" "public" {
+    vpc_id = aws_vpc.demo_vpc.id
+
+    tags = {
+        Name = "demo-public-nacl"
+    }
+}
+
+resource "aws_network_acl_rule" "inbound_https" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number = 100
+  egress = false
+  protocol = "tcp"
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+}
+
+resource "aws_network_acl_rule" "outbound_https" {
+    for_each = local.subnets.private
+    network_acl_id = aws_network_acl.public.id
+    rule_number = 100 + index(local.subnets.private, each.value)
+    egress = true
+    protocol = "tcp"
+    rule_action = "allow"
+    cidr_block = each.value
+    from_port = 443
+    to_port = 443
+}
+
+resource "aws_network_acl" "private" {
+    vpc_id = aws_vpc.demo_vpc.id
+
+    tags = {
+        Name = "demo-private-nacl"
+    }
+}
+
+resource "aws_network_acl_rule" "private_inbound_https" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number = 100
+  egress = false
+  protocol = "tcp"
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+}
+
+resource "aws_network_acl_rule" "private_outbound_https" {
+    for_each = local.subnets.private
+    network_acl_id = aws_network_acl.private.id
+    rule_number = 100 + index(local.subnets.private, each.value)
+    egress = true
+    protocol = "tcp"
+    rule_action = "allow"
+    cidr_block = each.value
+    from_port = 443
+    to_port = 443
+}
+
+
+# ------------------------
 # Security Group for RDS (Allow inbound ONLY from app SG)
 # ------------------------
 resource "aws_security_group" "rds_sg" {
